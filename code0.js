6f6910dc64c48356feef865250676db091fa7e47
Eduardoqm <eduardobio2009@gmail.com>
2020-06-17 18:18:46 -0300

    Test games

---
+++ code0.js
@@ -0,0 +1,21 @@
+gdjs.New_32sceneCode = {};
+
+gdjs.New_32sceneCode.conditionTrue_0 = {val:false};
+gdjs.New_32sceneCode.condition0IsTrue_0 = {val:false};
+
+
+gdjs.New_32sceneCode.eventsList0x5b7338 = function(runtimeScene) {
+
+}; //End of gdjs.New_32sceneCode.eventsList0x5b7338
+
+
+gdjs.New_32sceneCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.New_32sceneCode.eventsList0x5b7338(runtimeScene);
+return;
+
+}
+
+gdjs['New_32sceneCode'] = gdjs.New_32sceneCode;
